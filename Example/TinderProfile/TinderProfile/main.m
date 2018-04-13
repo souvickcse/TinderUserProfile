cb14d9042ae306ed0b940db67295e95eee8649a4
souvick <souvick@unifiedinfotech.net>
2018-04-13 16:28:07 +0530

    Example

---
+++ Example/TinderProfile/TinderProfile/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TinderProfile
+//
+//  Created by souvick on 12/04/18.
+//  Copyright © 2018 souvick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
