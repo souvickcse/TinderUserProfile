c2da93f4b459fa35b814b945e4c5424a25b5f41a
souvick <souvick@unifiedinfotech.net>
2018-04-13 16:24:19 +0530

    Initial Commit

---
+++ Example/TinderProfile/TinderProfile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TinderProfile
+//
+//  Created by souvick on 12/04/18.
+//  Copyright © 2018 souvick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/TinderProfile/TinderProfile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TinderProfile
+//
+//  Created by souvick on 12/04/18.
+//  Copyright © 2018 souvick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
